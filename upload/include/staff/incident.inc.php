029d9215f14931d5c6e1735790af4dfd2ab3b5a8
mSmitty1917 <melissasmith358@gmail.com>
2016-12-05 21:09:48 -0600

    IM-1 -testing vcs integration
    IM-1 -testing vcs integraton to be able to track changes in project
    management

---
+++ upload/include/staff/incident.inc.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: melissaSusan
+ * Date: 12/4/2016
+ * Time: 8:34 PM
+ */
+//Note that ticket obj is initiated in tickets.php.
+if(!defined('OSTSCPINC') || !$thisstaff || !is_object($ticket) || !$ticket->getId()) die('Invalid path');
+
+//Make sure the staff is allowed to access the page.
+if(!@$thisstaff->isStaff() || !$ticket->checkStaffPerm($thisstaff)) die('Access Denied');
+
+//Re-use the post info on error...savekeyboards.org (Why keyboard? -> some people care about objects than users!!)
+$info=($_POST && $errors)?Format::input($_POST):array();
+
+//Get the goodies.
+$dept  = $ticket->getDept();  //Dept
+$role  = $thisstaff->getRole($dept);
+$staff = $ticket->getStaff(); //Assigned or closed by..
+$user  = $ticket->getOwner(); //Ticket User (EndUser)
+$team  = $ticket->getTeam();  //Assigned team.
+$sla   = $ticket->getSLA();
+$lock  = $ticket->getLock();  //Ticket lock obj
+if (!$lock && $cfg->getTicketLockMode() == Lock::MODE_ON_VIEW)
+    $lock = $ticket->acquireLock($thisstaff->getId());
+$mylock = ($lock && $lock->getStaffId() == $thisstaff->getId()) ? $lock : null;
+$id    = $ticket->getId();    //Ticket ID.
+//Useful warnings and errors the user might want to know!
+if ($ticket->isClosed() && !$ticket->isReopenable())
+    $warn = sprintf(
+        __('Current ticket status (%s) does not allow the end user to reply.'),
+        $ticket->getStatus());
+elseif ($ticket->isAssigned()
+    && (($staff && $staff->getId()!=$thisstaff->getId())
+        || ($team && !$team->hasMember($thisstaff))
+    ))
+    $warn.= sprintf('&nbsp;&nbsp;<span class="Icon assignedTicket">%s</span>',
+        sprintf(__('Ticket is assigned to %s'),
+            implode('/', $ticket->getAssignees())
+        ));
+
+if (!$errors['err']) {
+
+    if ($lock && $lock->getStaffId()!=$thisstaff->getId())
+        $errors['err'] = sprintf(__('This ticket is currently locked by %s'),
+            $lock->getStaffName());
+    elseif (($emailBanned=Banlist::isBanned($ticket->getEmail())))
+        $errors['err'] = __('Email is in banlist! Must be removed before any reply/response');
+    elseif (!Validator::is_valid_email($ticket->getEmail()))
+        $errors['err'] = __('EndUser email address is not valid! Consider updating it before responding');
+}
+
+$unbannable=($emailBanned) ? BanList::includes($ticket->getEmail()) : false;
+
+if($ticket->isOverdue())
+    $warn.='&nbsp;&nbsp;<span class="Icon overdueTicket">'.__('Marked overdue!').'</span>';
+
+?>
