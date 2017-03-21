3be5b70409667aa5910f25052a2757aa69ad2d2e
Melissa S Smith <Melissa@cenovotechnologies.com>
2017-03-20 21:45:16 -0500

    IM-28 Task Page Layout -task popup layout

--- upload/include/staff/templates/task-preview.tmpl.php
+++ upload/include/staff/templates/task-preview.tmpl.php
@@ -1,6 +1,7 @@
 <?php
 $error=$msg=$warn=null;
 
+/** @var Task $task */
 if (!$task->checkStaffPerm($thisstaff))
      $warn.= __('You do not have access to this task');
 elseif ($task->isOverdue())
@@ -11,7 +12,7 @@ echo sprintf(
          <h2>'.__('Task #%s').': %s</h2>',
          $task->getNumber(),
          $task->getNumber(),
-         Format::htmlchars($task->getTitle()));
+         $task->getTitle());
 
 if($error)
     echo sprintf('<div id="msg_error">%s</div>',$error);
@@ -20,88 +21,92 @@ elseif($msg)
 elseif($warn)
     echo sprintf('<div id="msg_warning">%s</div>',$warn);
 
-echo '<ul class="tabs" id="task-preview">';
+echo '<ul class="nav nav-tabs" id="task-preview" role="tablist">';
 
 echo '
-        <li class="active"><a href="#summary"
-            ><i class="icon-list-alt"></i>&nbsp;'.__('Task Summary').'</a></li>';
+        <li class="nav-item">
+            <a class="nav-link active" data-toggle="tab" href="#summary" role="tab">
+                <i class="icon-list-alt"></i>&nbsp;'.__('Task Summary').'
+            </a>
+        </li>';
 if ($task->getThread()->getNumCollaborators()) {
     echo sprintf('
-        <li><a id="collab_tab" href="#collab"
-            ><i class="icon-fixed-width icon-group
-            faded"></i>&nbsp;'.__('Collaborators (%d)').'</a></li>',
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" id="collab_tab" href="#collab" role="tab">
+                <i class="icon-fixed-width icon-group faded"></i>&nbsp;'.__('Collaborators (%d)').'
+            </a>
+        </li>',
             $task->getThread()->getNumCollaborators());
 }
 echo '</ul>';
-echo '<div id="task-preview_container">';
-echo '<div class="tab_content" id="summary">';
-echo '<table border="0" cellspacing="" cellpadding="1" width="100%" class="ticket_info">';
+echo '<div class="tab-content" id="task-preview_container">';
+echo '<div class="tab-pane active" id="summary" role="tabpanel">';
+echo '<div class="col-sm-12">';
 $status=sprintf('<span>%s</span>',ucfirst($task->getStatus()));
 echo sprintf('
-        <tr>
-            <th width="100">'.__('Status').':</th>
-            <td>%s</td>
-        </tr>
-        <tr>
-            <th>'.__('Created').':</th>
-            <td>%s</td>
-        </tr>',$status,
-        Format::datetime($task->getCreateDate()));
+        <dl class="row">
+            <dt class="col-sm-4 sidebar-label align-middle">'.__('Status').'</dt>
+            <dd class="col-sm-8 sidebar-detail">%s</dd>
+        </dl>
+        <dl class="row">
+            <dt class="col-sm-4 sidebar-label align-middle">'.__('Created').'</dt>
+            <dd class="col-sm-8 sidebar-detail">%s</dd>
+        </dl>',$status,
+        $task->getCreateDate());
 
 if ($task->isClosed()) {
 
     echo sprintf('
-            <tr>
-                <th>'.__('Completed').':</th>
-                <td>%s</td>
-            </tr>',
-            Format::datetime($task->getCloseDate()));
+            <dl class="row">
+                <dt class="col-sm-4 sidebar-label align-middle">'.__('Completed').'</dt>
+                <dd class="col-sm-8 sidebar-detail">%s</dd>
+            </dl>',
+            $task->getCloseDate());
 
 } elseif ($task->isOpen() && $task->duedate) {
     echo sprintf('
-            <tr>
-                <th>'.__('Due Date').':</th>
-                <td>%s</td>
-            </tr>',
-            Format::datetime($task->duedate));
+            <dl class="row">
+                <dt class="col-sm-4 sidebar-label align-middle">'.__('Due Date').'</dt>
+                <dd class="col-sm-8 sidebar-detail">%s</dd>
+            </dl>',
+            $task->duedate);
 }
-echo '</table>';
+echo '</div>';
 
 
 echo '<hr>
-    <table border="0" cellspacing="" cellpadding="1" width="100%" class="ticket_info">';
+    <div class="col-sm-12">';
 if ($task->isOpen()) {
     echo sprintf('
-            <tr>
-                <th width="100">'.__('Assigned To').':</th>
-                <td>%s</td>
-            </tr>', $task->getAssigned() ?: ' <span class="faded">&mdash; '.__('Unassigned').' &mdash;</span>');
+            <dl class="row">
+                <dt class="col-sm-4 sidebar-label align-middle">'.__('Assigned To').'</dt>
+                <dd class="col-sm-8 sidebar-detail">%s</dd>
+            </dl>', $task->getAssigned() ?: ' <span class="faded">&mdash; '.__('Unassigned').' &mdash;</span>');
 }
 echo sprintf(
     '
-        <tr>
-            <th width="100">'.__('Department').':</th>
-            <td>%s</td>
-        </tr>',
-    Format::htmlchars($task->dept->getName())
-    );
+        <dl class="row">
+            <dt class="col-sm-4 sidebar-label align-middle">'.__('Department').'</dt>
+            <dd class="col-sm-8 sidebar-detail">%s</dd>
+        </dl>', $task->dept->getName());
 
 echo '
-    </table>';
+    </div>';
 echo '</div>';
 ?>
 <?php
 //TODO: add link to view if the user has permission
 ?>
-<div class="hidden tab_content" id="collab">
-    <table border="0" cellspacing="" cellpadding="1">
-        <colgroup><col style="min-width: 250px;"></col></colgroup>
+<div class="tab-pane" id="collab" role="tabpanel">
+    <div class="col-sm-12">
+        <div class="spacer"></div>
         <?php
+        /** @var Collaborator $collab */
         if (($collabs=$task->getThread()->getCollaborators())) {?>
         <?php
             foreach($collabs as $collab) {
-                echo sprintf('<tr><td %s><i class="icon-%s"></i>
-                        <a href="users.php?id=%d" class="no-pjax">%s</a> <em>&lt;%s&gt;</em></td></tr>',
+                echo sprintf('<div class="row"><label>%s<i class="icon-%s"></i>
+                        <a href="users.php?id=%d" class="no-pjax">%s</a><div class="font-italic">&lt;%s&gt;</div></label></div>',
                         ($collab->isActive()? '' : 'class="faded"'),
                         ($collab->isActive()? 'comments' :  'comment-alt'),
                         $collab->getUserId(),
@@ -111,7 +116,7 @@ echo '</div>';
         }  else {
             echo __("Task doesn't have any collaborators.");
         }?>
-    </table>
+    </div>
     <br>
     <?php
     echo sprintf('<span><a class="collaborators"
